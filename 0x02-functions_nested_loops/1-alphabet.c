e239e44add7db0f1970de35c7cafc8974fcb6a7e
GideonGichamba <gideongichamba@gmail.com>
2022-07-06 03:15:22 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,16 @@
 #include "main.h"
 /**
- * A program that prints alphabet
+ * main - program enrty point
+ *
+ * Description: A program that prints alphabet
+ *
+ * Return: success 0
 */
 void print_alphabet(void)
-{	
+{
 	char c;
 
-	for (c = 'a'; c <= 'z'; c++);
+	for (c = 'a'; c <= 'z'; c++)
 		_putchar(c);
 	_putchar('\n');
 }
