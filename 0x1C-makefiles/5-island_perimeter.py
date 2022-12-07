ed6079cf6f8f2e096ac18856498394c96c9eb225
GideonGichamba <gideongichamba@gmail.com>
2022-12-07 01:45:19 -0800

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
-"""Defines an island perimeter measuring function."""
+"""Module containing function to get perimeter of island"""
 
 
 def island_perimeter(grid):
-    """Return the perimiter of an island.
-    The grid represents water by 0 and land by 1.
-    Args:
-        grid (list): A list of list of integers representing an island.
-    Returns:
-        The perimeter of the island defined in grid.
-    """
-    width = len(grid[0])
-    height = len(grid)
-    edges = 0
-    size = 0
+    """Function to find the perimeter of an island"""
+    perimeter = 0
+    coord = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            if (grid[y][x] == 1):
+                if (y - 1 < 0):
+                    perimeter += 1
+                elif (grid[y - 1][x] == 0):
+                    perimeter += 1
 
-    for i in range(height):
-        for j in range(width):
-            if grid[i][j] == 1:
-                size += 1
-                if (j > 0 and grid[i][j - 1] == 1):
-                    edges += 1
-                if (i > 0 and grid[i - 1][j] == 1):
-                    edges += 1
-    return size * 4 - edges * 2
-    
+                if (y + 1 >= len(grid)):
+                    perimeter += 1
+                elif (grid[y + 1][x] == 0):
+                    perimeter += 1
+
+                if (x - 1 < 0):
+                    perimeter += 1
+                elif (grid[y][x - 1] == 0):
+                    perimeter += 1
+
+                if (x + 1 >= len(grid[0])):
+                    perimeter += 1
+                elif (grid[y][x + 1] == 0):
+                    perimeter += 1
+
+    return perimeter
